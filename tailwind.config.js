0cea3f88186469ebc3f50836275b49355d0171a5
sanjay-perisync <sanjay@perisync.work>
2025-02-13 14:40:29 +0530

    message

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBgBlack: "#1f1f1f", 
+        customleftblack:"#121212"
+      },
+    },
+  },
+  plugins: [],
+};
